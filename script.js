2fb14ac5c8468a0f3a10b5366482ac5f02f417a6
Bryan Medina <bryan.medina.per@gmail.com>
2022-06-26 22:27:08 -0500

    Add single round function, playable version

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
+function humanPlay(){
+    const options = ['rock', 'papper', 'scissors'];
+    const user = prompt('Enter rock, papper, scissors').toLowerCase();
+
+    if(options.includes(user)){
+        return user
+    }
+    else{
+        return 'input not valid';
+    }
+}
+
 function computerPlay(){
-    const options = ['Rock', 'Papper', 'Scissors'];
+    const options = ['rock', 'papper', 'scissors'];
     return options[Math.floor(Math.random()*3)];
 }
 
-console.log(computerPlay());
+function singleRound(){
+    const human = humanPlay();
+    const computer = computerPlay();
+
+    return `human: ${human}, computer: ${computer} \n${winningConditions(human, computer)}`;
+}
+
+function winningConditions(human, computer){
+    if(human === computer){
+        return "It's a draw!";
+    }
+    else if(human === 'rock' && computer === 'papper'){
+        return 'computer wins';
+    }
+    else if(human === 'papper' && computer === 'scissors'){
+        return 'computer wins';
+    }
+    else if(human === 'scissors' && computer === 'rock'){
+        return 'computer wins';
+    }
+    else{
+        return 'human wins';
+    }
+}
+
+console.log(singleRound());
